b383416878fb9d33c4088b8af88c9a149fdaf89e
MabelSitumorang <situmorangmabel@gmail.com>
2025-12-14 19:30:08 +0700

    small fix

--- resources/views/district-information/index.blade.php
+++ resources/views/district-information/index.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1" />
-    <title>Beranda - Toba Hita</title>
+    <title>Toba Hita</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}" />
 
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
